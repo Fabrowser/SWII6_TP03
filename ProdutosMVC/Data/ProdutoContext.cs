a542aa920f18653085d71c8f29647ae269fc9b4a
Fabrowser <fabriciocarlosguima@gmail.com>
2023-10-20 21:26:46 -0300

    Commit inicial - Projeto CRUD de produtos

---
+++ ProdutosMVC/Data/ProdutoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProdutosMVC.Models;
+
+
+
+namespace ProdutosMVC.Data
+{
+    public class ProdutoContext: DbContext
+    {
+        public DbSet<Produto> Departamentos { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog=TP03;Integrated Security = true");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+
+    }
+}
